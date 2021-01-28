bb73c5643ce0dbd9abf1909eb5469ec720856244
Furkan Demir <furkan.demir_@outlook.com>
2021-01-28 10:43:34 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,25 +1,25 @@
 #!/usr/bin/env node
 
-const request = require("request");
-const HTMLParser = require("node-html-parser");
-const chalk = require("chalk");
+const request = require("request")
+const HTMLParser = require("node-html-parser")
+const chalk = require("chalk")
 
-const word = process.argv.slice(2)[0];
+const word = process.argv.slice(2)[0]
 
 if (typeof word === "undefined") {
-  console.log(chalk.redBright("Lütfen bir sözcük giriniz."));
-  return;
+  console.log(chalk.redBright("Lütfen bir sözcük giriniz."))
+  return
 }
 
 request(
   "https://www.nisanyansozluk.com/?k=" + word.trim(),
   (error, response, body) => {
-    const root = HTMLParser.parse(body);
-    const rows = root.querySelectorAll(".hghlght .yaz .eskoken");
+    const root = HTMLParser.parse(body)
+    const rows = root.querySelectorAll(".hghlght .yaz .eskoken")
 
     if (error || rows.length === 0) {
-      console.log(chalk.redBright("Aradığınız sözcük bulunamadı."));
-      return;
+      console.log(chalk.redBright("Aradığınız sözcük bulunamadı."))
+      return
     }
 
     const result = {
@@ -35,20 +35,20 @@ request(
         title: "Ek Açıklama",
         text: null,
       },
-    };
+    }
 
-    result.history.text = rows[0].rawText;
+    result.history.text = rows[0].rawText
     result.description.text = rows.find((row) =>
       row.rawText.startsWith("Not:")
-    )?.rawText;
+    )?.rawText
     result.origin.text = root
       .querySelector("meta[property=og:description]")
-      .getAttribute("content");
+      .getAttribute("content")
 
     for (const key in result) {
-      if (!result[key].text) continue;
-      console.log(chalk.green(result[key].title + ":"));
-      console.log(chalk.white(result[key].text));
+      if (!result[key].text) continue
+      console.log(chalk.green(result[key].title + ":"))
+      console.log(chalk.white(result[key].text))
     }
   }
-);
+)
